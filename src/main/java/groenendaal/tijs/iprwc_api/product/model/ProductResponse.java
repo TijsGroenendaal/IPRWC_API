1a6ca6cd27d963bc9a4ffa08c46e9afad19bf9a5
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-01-04 14:54:03 +0100

    🩹 add id to productResponse

--- src/main/java/groenendaal/tijs/iprwc_api/product/model/ProductResponse.java
+++ src/main/java/groenendaal/tijs/iprwc_api/product/model/ProductResponse.java
@@ -2,15 +2,19 @@ package groenendaal.tijs.iprwc_api.product.model;
 
 import lombok.Value;
 
+import java.util.UUID;
+
 @Value
 public class ProductResponse {
 
+   UUID id;
    String name;
    int quantity;
    float price;
    String description;
 
    public ProductResponse(ProductEntity productEntity) {
+      this.id = productEntity.getId();
       this.description = productEntity.getDescription();
       this.name = productEntity.getName();
       this.price = productEntity.getPrice();
